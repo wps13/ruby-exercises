b8b8c8299dd95e4ab4d5f667928435fcdbcfe550
Willane Paiva <willanepaiva@hotmail.com>
2019-04-25 14:19:04 -0300

    #11 - Add 01/mean-length

---
+++ exercicios-01/mean-length.rb
@@ -0,0 +1,12 @@
+# Exercise 03
+
+# Given two values, calcule the mean and return the mean length
+
+num_1 = 9.23
+
+num_2 = 8.32
+
+mean = (num_1+num_2)/2
+
+
+puts "Your mean is " + mean.to_s + " and its length is " + (mean.to_s.size - 1).to_s
