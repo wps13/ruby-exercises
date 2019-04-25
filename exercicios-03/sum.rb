7a8bdcf233d2251c3b25be41d79b944224a1531d
Willane Paiva <willanepaiva@hotmail.com>
2019-04-25 16:22:22 -0300

    #15 - Add 03/sum

---
+++ exercicios-03/sum.rb
@@ -0,0 +1,15 @@
+# Exercicio 4
+
+# Ask the user to input two numbers, printing the resulting sum
+
+puts "Hello, insert the first number below:"
+
+num_1 = gets.to_i
+
+puts "Now, insert the second number:"
+
+num_2 = gets.to_i
+
+total = num_1+num_2
+
+puts "The sum of #{num_1} and #{num_2} is #{total}"
