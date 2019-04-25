15d18f2b4df9225f8b4140bc049a315b6df5050c
Willane Paiva <willanepaiva@hotmail.com>
2019-04-25 13:50:24 -0300

    #09 - Add 01/name-length

---
+++ exercicios-01/name-length.rb
@@ -0,0 +1,11 @@
+# Exercise 01 
+
+# Receive a string that represents a name and check its length
+
+puts "Insert your name to check its size:"
+
+name = gets
+
+size = name.length
+
+puts "Your name size is: " + size.to_s
