11d89e8bea1b843a7ce6e26200546e5f0e8a5962
Willane Paiva <willanepaiva@hotmail.com>
2019-04-25 15:38:48 -0300

    #12 - Add 01/itens-list

---
+++ exercicios-01/itens-list.rb
@@ -0,0 +1,17 @@
+# Exercise 9
+
+# Given the itens list, return the total amount of ingredients
+
+item1 = "6 ovos"
+
+item2 = "1 quilo de farinha"
+
+item3 = "1 litro de leite"
+
+item4 = "2 tabletes de manteiga"
+
+item5 = "3 xicaras de açúcar"
+
+total = item1.to_f + item2.to_f + item3.to_f + item4.to_f + item5.to_f
+
+puts total
