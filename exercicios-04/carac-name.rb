a042a2e91c12c9471bc05e42830e76a089678878
Willane Paiva <willanepaiva@hotmail.com>
2019-05-03 13:29:43 -0300

    #21 - Add 04/carac-name

---
+++ exercicios-04/carac-name.rb
@@ -0,0 +1,11 @@
+# Exercise 06
+
+# Calculate the characters of a given name
+
+print "What's your name? "
+
+name = gets.chomp
+
+len = name.length
+
+puts "Hello, #{name}, your name has #{len} characters."
