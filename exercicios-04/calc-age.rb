38a9cb0ce74ad0f639e45e1439d053e096bfbaab
Willane Paiva <willanepaiva@hotmail.com>
2019-05-03 12:54:20 -0300

    #19 - Add 04/calc-age

---
+++ exercicios-04/calc-age.rb
@@ -0,0 +1,18 @@
+# Exercise 03
+
+# Given the user name and year that was born, calculate the age.
+# Example:
+# inputs: João, 1979 
+# Outputs: Hello João, you are 40 years old
+
+print "Hello, what's your name? "
+
+name = gets
+
+print "In which year was you born? "
+
+year = gets.to_i
+
+age = 2019 - year
+
+puts "Hello, #{name}.You are #{age} years old."
