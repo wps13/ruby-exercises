b83e703e1c8c82c6660bb1cb843218d817bce6e1
Willane Paiva <willanepaiva@hotmail.com>
2019-05-03 13:02:42 -0300

    #20 - Add 04/room-area

---
+++ exercicios-04/room-area.rb
@@ -0,0 +1,14 @@
+#  Exercise 04
+
+# Calculate the area of any room, given the width and the deepth
+
+print "What is the width of the room? "
+
+width = gets.to_f
+
+print "What's the deepth? "
+
+deepth = gets.to_f
+
+area = width * deepth
+puts "The room's area is #{area} meters"
