7640eec3c364bca93bb34c4fec9b6423ea63320e
Willane Paiva <willanepaiva@hotmail.com>
2019-05-04 20:54:47 -0300

    #24 - Add 05/equal-ten

---
+++ exercicios-05/equal-ten.rb
@@ -0,0 +1,14 @@
+# Exercise 01
+
+# Given a number, tell if the number is equal to ten or not 
+
+print "Insert the number to check: "
+
+nbr = gets.to_i
+
+if nbr == 10 then
+  puts "The number is equal to 10"
+else
+  puts "The number is different from 10"
+
+end
