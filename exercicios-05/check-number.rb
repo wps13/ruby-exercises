981b24ebdf60ca0c75b5ff62522e0e9a6ad97cef
Willane Paiva <willanepaiva@hotmail.com>
2019-05-04 21:36:39 -0300

    #30 - Add 05/check-number

---
+++ exercicios-05/check-number.rb
@@ -0,0 +1,13 @@
+# Exercise 07
+
+# Given a number, print if it is positive or negative
+
+print "What number you want to check? "
+
+number = gets.to_i
+
+if number > 0 then
+  puts "The number is positive"
+else
+  puts "The number is negative"
+end
