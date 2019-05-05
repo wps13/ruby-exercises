e7152d0f514f046d6ff75d1330b887474bd9831e
Willane Paiva <willanepaiva@hotmail.com>
2019-05-04 21:01:16 -0300

    #26 - Add 05/equal-numbers

---
+++ exercicios-05/equal-numbers.rb
@@ -0,0 +1,18 @@
+# Exercise 04
+
+# Tell the user if the given numbers are equal or not
+
+print "Insert the first number to compare: "
+
+number_1 = gets.to_i
+
+print "Insert the second number to compare: "
+
+number_2 = gets.to_i
+
+if number_1 == number_2 then
+  puts "The numbers are equal"
+else
+  puts "The numbers are different"
+
+end
