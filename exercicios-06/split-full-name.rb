e9f7c59a89688fedfa38604b375b1ed9fe665f06
Willane Paiva <willanepaiva@hotmail.com>
2019-05-06 14:07:15 -0300

    #39 - Add 06/split-full-name

---
+++ exercicios-06/split-full-name.rb
@@ -0,0 +1,12 @@
+# Exercise 
+
+# Given a full name, print the first and last one
+
+print "What's your full name? "
+
+name = gets
+
+
+split_name = name.split(" ")
+split_length = split_name.length
+print "The shortened name is #{split_name[0]} #{split_name[split_length-1]}\n" 
