18e4f4bad379cefd770e50cb6efdbf5684813691
Willane Paiva <willanepaiva@hotmail.com>
2019-05-06 18:53:16 -0300

    #41 - Add 07/repeat-name

---
+++ exercicios-07/repeat-name.rb
@@ -0,0 +1,11 @@
+# Exercise 02
+
+# Given an integer n and a name, print the name n times.
+
+print "Insert the name to be repeated: "
+name = gets
+
+print "Insert how many times you wanna to print it: "
+n = gets.to_i
+
+puts name*n
